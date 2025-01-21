66df156cfec27d7d9888ca5709edf4d1348c3f81
Chonnawee <chonnawee.b25@gmail.com>
2025-01-21 15:46:02 +0700

    add: add GetBeer function

--- usecases/beers_repository.go
+++ usecases/beers_repository.go
@@ -11,6 +11,7 @@ type GetDatasParams struct {
 type BeersRepository interface {
 	Create(beer entities.Beers) error
 	GetDatas(params GetDatasParams) (beers []entities.Beers, err error)
+	GetData(id uint64) (*entities.Beers, error)
 	Update(id uint64, beer entities.Beers) error
 	Delete(id uint64) error
 }
