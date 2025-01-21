824d27191692167659a989995d72b6ec63870f63
Chonnawee <chonnawee.b25@gmail.com>
2025-01-21 15:46:21 +0700

    chore: edit db tag

--- entities/beers.go
+++ entities/beers.go
@@ -6,8 +6,8 @@ type Beers struct {
 	ID           uint64    `gorm:"primaryKey;comment:รหัสชื่อเบียร์"`
 	BeerTypeName string    `gorm:"column:beer_type_name;not null;comment:ชื่อประเภทเบียร์"`
 	BeerName     string    `gorm:"column:beer_name;not null;comment:ชื่อเบียร์"`
-	BeerDesc     string    `gorm:"column:beer_desc;not null;comment:รายละเอียดเบียร์"`
-	BeerImgPath  string    `gorm:"column:beer_img_path;not null;comment:path รูปเบียร์"`
+	BeerDesc     string    `gorm:"column:beer_desc;comment:รายละเอียดเบียร์"`
+	BeerImgPath  string    `gorm:"column:beer_img_path;comment:path รูปเบียร์"`
 	CreatedAt    time.Time `gorm:"column:created_at;comment:วันที่สร้างข้อมูล"`
 	UpdatedAt    time.Time `gorm:"column:updated_at;comment:วันที่แก้ไขข้อมูล"`
 }
