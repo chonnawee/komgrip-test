93a31c96647374d00f33e6224fe3f08f94d9f558
Chonnawee <chonnawee.b25@gmail.com>
2025-01-21 14:28:30 +0700

    refac: add beer_type_name & remove beer_type_id

--- entities/beers.go
+++ entities/beers.go
@@ -1,10 +1,14 @@
 package entities
 
+import "time"
+
 type Beers struct {
-	ID          uint64    `gorm:"primaryKey;comment:รหัสชื่อเบียร์"`
-	BeerTypeID  uint64    `gorm:"column:beer_type_id;not null;comment:รหัสประเภทเบียร์"`
-	BeerName    string    `gorm:"column:beer_name;not null;comment:ชื่อเบียร์"`
-	BeerDesc    string    `gorm:"column:beer_desc;not null;comment:รายละเอียดเบียร์"`
-	BeerImgPath string    `gorm:"column:beer_img_path;not null;comment:path รูปเบียร์"`
-	BeerType    BeerTypes `gorm:"foreignKey:BeerTypeID"`
+	ID           uint64    `gorm:"primaryKey;comment:รหัสชื่อเบียร์"`
+	BeerTypeName string    `gorm:"column:beer_type_name;not null;comment:ชื่อประเภทเบียร์"`
+	BeerName     string    `gorm:"column:beer_name;not null;comment:ชื่อเบียร์"`
+	BeerDesc     string    `gorm:"column:beer_desc;not null;comment:รายละเอียดเบียร์"`
+	BeerImgPath  string    `gorm:"column:beer_img_path;not null;comment:path รูปเบียร์"`
+	CreatedAt    time.Time `gorm:"column:created_at;comment:วันที่สร้างข้อมูล"`
+	UpdatedAt    time.Time `gorm:"column:updated_at;comment:วันที่แก้ไขข้อมูล"`
+	BeerType     BeerTypes `gorm:"foreignKey:BeerTypeID"`
 }
