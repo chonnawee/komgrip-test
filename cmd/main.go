2a885c40e03b81156588b5c3393be01fc5700552
Chonnawee <chonnawee.b25@gmail.com>
2025-01-21 15:44:37 +0700

    chore: instance all service

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"komgrip-test/adapters"
+	"komgrip-test/entities"
+	"komgrip-test/usecases"
 	"log"
 	"os"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+
+var port string
 
 func init() {
 	err := godotenv.Load(".env")
@@ -17,7 +24,12 @@ func init() {
 	}
 }
 
+func initializeConfig() {
+	port = os.Getenv("APP_PORT")
+}
+
 func main() {
+	initializeConfig()
 	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USERNAME"),
 		os.Getenv("DB_PASSWORD"),
@@ -29,7 +41,22 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	_ = db
-
 	fmt.Println("db connected")
+
+	db.AutoMigrate(entities.Beers{})
+
+	app := fiber.New(fiber.Config{
+		JSONEncoder: json.Marshal,
+		JSONDecoder: json.Unmarshal,
+	})
+
+	uow := adapters.NewUnitOfWorkDB(db)
+	usecase := usecases.NewBeersService(uow)
+	handler := adapters.NewBeersHandler(usecase)
+
+	app.Post("/beer", handler.CreateBeer)
+	app.Get("/beer", handler.GetBeers)
+	app.Put("/beer/:id", handler.UpdateBeer)
+	app.Delete("/beer/:id", handler.DeleteBeer)
+	app.Listen(":" + port)
 }
